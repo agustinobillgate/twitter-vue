71b89cc3efe60ff911d803ccd72b38fe28b05e46
agustinobillgate <agustinobillgate@gmail.com>
2022-10-08 12:26:56 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+    data() {
+        return {
+            profiles: [
+                {
+                    username: 'marisahonkai', name: 'Honkai News Network', desc: 'Honkai Impact 3rd and Honkai Star Rail CN News, Translation and Guides', created: 'July 2010', following: 305, follower: 170
+                }
+            ]
+        }
+    }
+})
+    .component('twitter-profile', {
+        template: '#twitter-profile-card',
+        props: {
+            username: { type: String },
+            name: { type: String },
+            desc: { type: String },
+            created: { type: String },
+            following: { type: Number },
+            follower: { type: Number }
+        }
+    })
+    .mount('#app')
